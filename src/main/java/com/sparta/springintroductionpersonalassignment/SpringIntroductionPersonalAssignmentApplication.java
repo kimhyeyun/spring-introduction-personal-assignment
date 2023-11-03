a7ccafda179bcfad5e6f679c0698b615128bf76c
Kimhyeyun <cqqwer@gmail.com>
2023-11-03 20:36:42 +0900

    init: project 생성

---
+++ src/main/java/com/sparta/springintroductionpersonalassignment/SpringIntroductionPersonalAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springintroductionpersonalassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionPersonalAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroductionPersonalAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springintroductionpersonalassignment/SpringIntroductionPersonalAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springintroductionpersonalassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductionPersonalAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
