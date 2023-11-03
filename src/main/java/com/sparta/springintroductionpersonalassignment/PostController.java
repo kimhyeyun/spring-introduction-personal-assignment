56e0a13af01e4beccebdbe418ca6c88611ba9daf
Kimhyeyun <cqqwer@gmail.com>
2023-11-03 21:30:58 +0900

    feat: repository, service, controller 틀 작성

---
+++ src/main/java/com/sparta/springintroductionpersonalassignment/PostController.java
@@ -0,0 +1,14 @@
+package com.sparta.springintroductionpersonalassignment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+}
